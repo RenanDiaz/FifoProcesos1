072f6700235030508c6588f42debae2c225c80a9
RenanDiaz <renandiazreyes@Nanchos-iMac.local>
2015-06-11 07:14:51 -0500

    Initial Commit

---
+++ FifoProcesos1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FifoProcesos1
+//
+//  Created by Renán Díaz Reyes on 6/11/15.
+//  Copyright (c) 2015 Renán Díaz Reyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
